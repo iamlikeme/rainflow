64e77c3d1b7b4920ed85ac9f8e1b5e7ad07cb80c
Piotr Janiszewski <i.am.like.me@gmail.com>
2016-03-30 20:44:02 +0200

    Add more metadata to setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from distutils.core import setup
 
 setup(name='rainflow',
       version='1.0',
-      description='Rainflow cycle counting algorythm according to ASTM E1049-85',
       author='Piotr Janiszewski',
       author_email='i.am.like.me@gmail.com',
       url='https://github.com/iamlikeme/rainflow/',
+      description='Rainflow cycle counting algorythm according to ASTM E1049-85',
+      license='GPLv3',
       py_modules=['rainflow'],
      )
