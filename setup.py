64bf3bda2a493c9122d318bc21952b6e65fee201
Piotr Janiszewski <piotr.janiszewski@inmeta.no>
2018-02-25 16:19:22 +0100

    Prepare for uploading to PyPI (#8)
    * Prepare for PyPI
    * Update .gitignore

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 from distutils.core import setup
 
-LICENSE = open("LICENSE").read()
-LONG_DESCRIPTION = open("README.md").read()
-
-setup(name='rainflow',
-      version='2.0.0',
-      author='Piotr Janiszewski',
-      author_email='i.am.like.me@gmail.com',
-      url='https://github.com/iamlikeme/rainflow/',
-      description='Rainflow cycle counting algorythm according to ASTM E1049-85',
-      long_description=LONG_DESCRIPTION,
-      license=LICENSE,
-      py_modules=['rainflow'],
-     )
+setup(
+    name='rainflow',
+    py_modules=['rainflow'],
+    version='2.0.0',
+    description='Implementation of ASTM E1049-85 rainflow cycle counting algorythm',
+    author='Piotr Janiszewski',
+    author_email='i.am.like.me@gmail.com',
+    url='https://github.com/iamlikeme/rainflow/',
+    download_url='https://github.com/iamlikeme/rainflow/archive/2.0.0.tar.gz',
+    classifiers=[],
+)
