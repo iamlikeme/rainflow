bfe3b96ee67979681da6c5868c7a425d3fd5026a
Piotr Janiszewski <iamlikeme@users.noreply.github.com>
2020-04-14 22:48:30 +0200

    Add setuptools_scm and importlib_metadata to requirements

--- setup.py
+++ setup.py
@@ -1,26 +1,7 @@
 from setuptools import setup
 import os
 
-
-def load_version(path):
-    with open(path, "rb") as f:
-        lines = f.read().decode("utf-8").splitlines()
-
-    for line in lines:
-        d = {}
-        try:
-            exec(line, d)
-            return d["__version__"]
-        except KeyError:
-            continue
-        except Exception:
-            continue
-    raise Exception("__version__ not found in {}".format(path))
-
-
 this_dir = os.path.dirname(__file__)
-
-version = load_version(path=os.path.join(this_dir, "src", "rainflow.py"))
 
 with open(os.path.join(this_dir, "README.md"), "rb") as fo:
     long_description = fo.read().decode("utf8")
@@ -30,20 +11,17 @@ setup(
     name='rainflow',
     package_dir={"": "src"},
     py_modules=['rainflow'],
-    version=version,
     description='Implementation of ASTM E1049-85 rainflow cycle counting algorithm',
+    install_requires=["importlib_metadata ; python_version < '3.8'"],
     extras_require={"dev": ["pytest ~= 4.6"]},
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4",
+    setup_requires=["setuptools_scm"],
+    use_scm_version=True,
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='Piotr Janiszewski',
-    author_email='i.am.like.me@gmail.com',
     url='https://github.com/iamlikeme/rainflow/',
     license="MIT",
-    download_url=(
-        'https://github.com/iamlikeme/rainflow/archive/v{}.tar.gz'
-        .format(version)
-    ),
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Science/Research",
